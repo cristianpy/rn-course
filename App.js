655bf55383fb7ad266c127d05c808080e56536d2
cristianpy <cristianramonpaniagua@gmail.com>
2018-10-04 15:15:08 -0400

    Add TextInput in App.js

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
+  state = {
+    placeName: ''
+  };
+
+  placeNameChangedHandler = (val) => {
+    this.setState({
+      placeName: val
+    });
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput 
+          style={{width: 300, borderColor: "black", borderWidth: 1}}
+          value={this.state.placeName} 
+          onChangeText={this.placeNameChangedHandler}
+        />
       </View>
     );
   }
